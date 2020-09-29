26cbd9fbdff72eaa42772687d9ea040d8338a9b7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-09-29 18:22:57 -0500

    [+] Controller > User: Check if the user is registered before inserting it into the Database

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,21 @@
-const { request } = require("express");
+const { request } = require( 'express' );
 
 const User = require( '../models/User' );
 
 exports .new = async ( request, response ) => {
-    // console .log( 'Controller: New User', request .body );
+    
+    const { email } = request .body;
 
-    const user = await new User( request .body );
-    user .save();
+    let user = await User .findOne({ email });  //  User query by email in the Database
+    
+    if( user ) {                                //  Check if the user is registered
+        return response .status( 400 ) .json({
+            msg: 'Registered user'
+        });
+    }
+
+    user = await new User( request .body );
+    user .save();                               //  Insert a new user to the Database
 
     response .json({
         msg: 'User created successfully!'
